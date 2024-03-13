66829d51571e8ea3e4f6c3f30e5add75d8c7f676
bucketsplash <garv_bhasin@brown.edu>
2024-03-13 12:07:21 -0400

    GUI changes to the design and created matrix for card dealer

---
+++ Blackjack_project.m
@@ -0,0 +1,19 @@
+prompt = "How many players would you like to play Blackjack today? ";
+number_of_players = input(prompt);
+black_jack_counter = 1 : 52;
+
+
+if number_of_players > 7
+    disp("We are sorry but the number of players currently playing Blackjack" + ...
+        " exceeds the recommended number of players. Please enter a valid number of players. ")
+    number_of_players = input(prompt);
+end
+
+for ii = 1 : number_of_players
+end
+
+
+for ii = 2 : 10
+    a = []
+    a = [A 1 2 3 4; ii 1 2 3 4]
+end
